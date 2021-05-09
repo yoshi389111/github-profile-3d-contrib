bf4db36adad3cfa798bb042d43085da275ca9dc7
sato yoshiyuki <yoshi.389111@gmail.com>
2021-05-09 13:15:33 +0900

    change: use vercel/ncc

--- dist/hello.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-function hello(name) {
-    return `Hello, ${name}!`;
-}
-exports.hello = hello;
-//# sourceMappingURL=hello.js.map
