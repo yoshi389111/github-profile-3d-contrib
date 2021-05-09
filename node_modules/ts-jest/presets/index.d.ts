7c40a667879dee38092d9df803c5e21410031608
sato yoshiyuki <yoshi.389111@gmail.com>
2021-05-09 13:26:51 +0900

    remove node_modules

--- node_modules/ts-jest/presets/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { TsJestPresets } from '../dist/types'
-
-declare const _default: {
-  defaults: TsJestPresets;
-  jsWithTs: TsJestPresets;
-  jsWithBabel: TsJestPresets;
-};
-export = _default;
