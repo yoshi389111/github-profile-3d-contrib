ee338cd8eeca55bdfdcce481ed421d9641033c8c
sato yoshiyuki <yoshi.389111@gmail.com>
2022-02-12 18:51:57 +0900

    feat: support for env `SETTING_JSON`

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as aggregate from './aggregate-user-info';
 import * as template from './color-template';
 import * as create from './create-svg';
 import * as f from './file-writer';
+import * as r from './settings-reader';
 import * as client from './github-graphql';
 
 export const main = async (): Promise<void> => {
@@ -29,59 +30,66 @@ export const main = async (): Promise<void> => {
         const response = await client.fetchData(token, userName, maxRepos);
         const userInfo = aggregate.aggregateUserInfo(response);
 
-        const settings = userInfo.isHalloween
-            ? template.HalloweenSettings
-            : template.NormalSettings;
+        if (process.env.SETTING_JSON) {
+            const settings = r.readSettingJson(process.env.SETTING_JSON);
+            f.writeFile(
+                'profile-customize.svg',
+                create.createSvg(userInfo, settings, false)
+            );
+        } else {
+            const settings = userInfo.isHalloween
+                ? template.HalloweenSettings
+                : template.NormalSettings;
 
-        const svgString1 = create.createSvg(
-            userInfo,
-            template.NorthSeasonSettings,
-            true
-        );
-        f.writeFile('profile-season-animate.svg', svgString1);
+            f.writeFile(
+                'profile-green-animate.svg',
+                create.createSvg(userInfo, settings, true)
+            );
+            f.writeFile(
+                'profile-green.svg',
+                create.createSvg(userInfo, settings, false)
+            );
 
-        const svgString2 = create.createSvg(userInfo, settings, true);
-        f.writeFile('profile-green-animate.svg', svgString2);
+            // Northern hemisphere
+            f.writeFile(
+                'profile-season-animate.svg',
+                create.createSvg(userInfo, template.NorthSeasonSettings, true)
+            );
+            f.writeFile(
+                'profile-season.svg',
+                create.createSvg(userInfo, template.NorthSeasonSettings, false)
+            );
 
-        const svgString3 = create.createSvg(
-            userInfo,
-            template.NorthSeasonSettings,
-            false
-        );
-        f.writeFile('profile-season.svg', svgString3);
+            // Southern hemisphere
+            f.writeFile(
+                'profile-south-season-animate.svg',
+                create.createSvg(userInfo, template.SouthSeasonSettings, true)
+            );
+            f.writeFile(
+                'profile-south-season.svg',
+                create.createSvg(userInfo, template.SouthSeasonSettings, false)
+            );
 
-        const svgString4 = create.createSvg(userInfo, settings, false);
-        f.writeFile('profile-green.svg', svgString4);
+            f.writeFile(
+                'profile-night-view.svg',
+                create.createSvg(userInfo, template.NightViewSettings, true)
+            );
 
-        // Southern hemisphere
-        f.writeFile(
-            'profile-south-season-animate.svg',
-            create.createSvg(userInfo, template.SouthSeasonSettings, true)
-        );
-        f.writeFile(
-            'profile-south-season.svg',
-            create.createSvg(userInfo, template.SouthSeasonSettings, false)
-        );
+            f.writeFile(
+                'profile-night-green.svg',
+                create.createSvg(userInfo, template.NightGreenSettings, true)
+            );
 
-        f.writeFile(
-            'profile-night-view.svg',
-            create.createSvg(userInfo, template.NightViewSettings, true)
-        );
+            f.writeFile(
+                'profile-night-rainbow.svg',
+                create.createSvg(userInfo, template.NightRainbowSettings, true)
+            );
 
-        f.writeFile(
-            'profile-night-green.svg',
-            create.createSvg(userInfo, template.NightGreenSettings, true)
-        );
-
-        f.writeFile(
-            'profile-night-rainbow.svg',
-            create.createSvg(userInfo, template.NightRainbowSettings, true)
-        );
-
-        f.writeFile(
-            'profile-gitblock.svg',
-            create.createSvg(userInfo, template.GitBlockSettings, true)
-        );
+            f.writeFile(
+                'profile-gitblock.svg',
+                create.createSvg(userInfo, template.GitBlockSettings, true)
+            );
+        }
     } catch (error) {
         console.error(error);
         core.setFailed('error');
