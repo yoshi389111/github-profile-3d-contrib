0c9051f5d6b34298d375344618aea9bbf4e066b1
sato yoshiyuki <yoshi.389111@gmail.com>
2022-08-08 08:48:53 +0900

    add: test case

--- spec/settings-reader.spec.ts
+++ spec/settings-reader.spec.ts
@@ -1,10 +1,25 @@
 import { readSettingJson } from "../src/settings-reader";
 
 describe('settings-reader', () => {
-    it('readSettingJson', () => {
+    it('readSettingJson(Single Settings)', () => {
         const settings = readSettingJson('./spec/settings.json');
-        expect(settings.type).toEqual('normal');
-        expect(settings.backgroundColor).toEqual('#ffffff');
-        expect(settings.foregroundColor).toEqual('#00000f');
+        if ("type" in settings) {
+            expect(settings.type).toEqual('normal');
+            expect(settings.backgroundColor).toEqual('#ffffff');
+            expect(settings.foregroundColor).toEqual('#00000f');
+        } else {
+            throw new Error("invalid value")
+        }
+    });
+
+    it('readSettingJson(Multiple Settings)', () => {
+        const settings = readSettingJson('./spec/settings2.json');
+        if ("length" in settings) {
+            expect(settings.length).toEqual(2);
+            expect(settings[0].type).toEqual('normal');
+            expect(settings[1].type).toEqual('rainbow');
+        } else {
+            throw new Error("invalid value")
+        }
     });
 });
